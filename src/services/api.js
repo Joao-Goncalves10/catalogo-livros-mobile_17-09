6f08bbf7f8c451879d2ee031d99f6a1ff56de9fb
Joao-Goncalves10 <joao.l.goncalves@aluno.senai.br>
2026-09-17 16:04:47 -0300

    criação do repositório e implementação de parte dos exercícios

--- src/services/api.js
+++ src/services/api.js
@@ -2,19 +2,57 @@
 const BASE_URL = "http://10.0.2.2:3000";
 
 export async function buscarLivros() {
-  // TODO: implementar
+  try {
+    const response = await fetch(`${BASE_URL}/livros`);
+    if (!response.ok) {
+      throw new Error(`Erro ${response.status}: falha ao buscar livros`)
+    }
+    return response.json();
+  } catch (e) {
+    console.error("buscarLivro:", e.message)
+  }
 }
 
 export async function buscarLivroPorId(id) {
-  // TODO: implementar
+
+  try {
+    const response = await fetch(`${BASE_URL}/livros/${id}`);
+    if (!response.ok) {
+      throw new Error(`Erro ${response.status}: livro não encontrado`)
+    }
+    return response.json();
+  } catch (e) {
+    console.error("buscarLivroId:", e.message)
+  }
 }
 
 export async function adicionarFavorito(livroId, observacao) {
-  // TODO: implementar
+  try {
+    const response = await fetch(`${BASE_URL}/favoritos`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ livroId, observacao })
+    });
+
+    if (!response.ok) {
+      const corpo = await response.json().catch(() => ({}));
+      const error = new Error(`Erro ${response.status}: falha ao adicionar favorito`);
+    }
+
+  } catch (e) {
+    console.error("adicionarFavorito:", e.message)
+  }
 }
 
 export async function listarFavoritos() {
-  // TODO: implementar
+  try {
+    const response = await fetch(`${BASE_URL}/favoritos`);
+    if (!response.ok) {
+      throw new Error(`Erro: ${response.status}: falha ao listar favoritos`)
+    }
+  } catch (e) {
+    console.error("listarFavoritos:", e.message)
+  }
 }
 
 export async function editarFavorito(id, observacao) {
